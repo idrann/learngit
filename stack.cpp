aaac779cf36f50571e0d4d8f85f9dd079fe65b0c
“idrann” <1078154930@qq.com>
2020-09-23 19:56:49 +0800

    initial project version

---
+++ stack.cpp
@@ -0,0 +1,33 @@
+#include"stack.h"
+
+Stack::Stack()
+{
+	top = 0;
+}
+
+bool Stack::isempty()const {return top == 0; }
+bool Stack::isfull()const {return top == MAX;} //范围是0,max-1
+bool Stack::push(const Item &item)
+{
+	if(top < MAX)
+	{
+		items[top++]=item;
+		return true;
+	}
+	else return false;
+}
+bool Stack::pop(Item& item)
+{
+	if(top > 0)
+	{
+		item = items[--top];
+		return true;
+	}
+	else return false;
+}
+Item& Stack::topval()
+{
+	if(top>0) return items[--top];
+//	else return 10010;
+}
+
---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include"stack.h"
+using namespace std;
+int main()
+{
+	Stack a;
+	Item b;
+	a.push(3);
+	a.push(5);
+	a.pop(b);
+	cout<<b<<endl;
+	a.push(0);
+	a.pop(b);
+	cout<<b<<endl;
+	return 0;
+	
+}
